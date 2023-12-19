c7b4fbda3a809f0943c98a6884a0c9f457a9e909
Antonio Silva <antoniopsilva083@gmail.com>
2023-12-19 02:03:32 -0300

    feat: create payment entity

---
+++ src/main/java/br/com/alurafood/spring/payments/entities/Payment.java
@@ -0,0 +1,63 @@
+package br.com.alurafood.spring.payments.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.validator.constraints.Length;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "payments")
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID paymentId;
+
+    @NotNull
+    @Positive
+    private BigDecimal value;
+
+    @NotBlank
+    @Size(max = 100)
+    private String name;
+
+    @NotBlank
+    @Size(max = 19)
+    private String number;
+
+    @NotBlank
+    @Size(max = 7)
+    private String expiration;
+
+    @NotBlank
+    @Size(min = 3, max = 3)
+    private String code;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @NotNull
+    private UUID orderId;
+
+    @NotNull
+    private UUID formOfPaymentId;
+
+    @CreationTimestamp
+    private Instant createdAt;
+
+    @UpdateTimestamp
+    private Instant updatedAt;
+}
