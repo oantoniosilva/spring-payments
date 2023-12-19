80052fa95c18e27e6efa17a25e357fdae8ca0d95
Antonio Silva <antoniopsilva083@gmail.com>
2023-12-19 00:44:32 -0300

    chore: start project

---
+++ src/main/java/br/com/alurafood/spring/payments/SpringPaymentsApplication.java
@@ -0,0 +1,13 @@
+package br.com.alurafood.spring.payments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPaymentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPaymentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alurafood/spring/payments/SpringPaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alurafood.spring.payments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPaymentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
